7dc0f720333a334b7cd5546304bd8e0367d7620c
AlanJGG <aguzmang2001@alumno.ipn.mx>
2022-03-23 21:57:10 -0600

    Algoritmia

--- z05AlgoritmiaJS/JS/logica.js
+++ z05AlgoritmiaJS/JS/logica.js
@@ -1,6 +1,31 @@
 
 
-//problema 3
+function problema1(){
+    var p1_input = document.getElementById('p1-input').value;
+    var p1_palabras = p1_input.split(' ');
+    
+    document.getElementById('p1-output').textContent = p1_palabras.reverse();
+}
+
+function problema2(){
+
+    var p2_x1 = document.getElementById('p2-x1').value;
+    var p2_x2 = document.getElementById('p2-x2').value;
+    var p2_x3 = document.getElementById('p2-x3').value;
+    var p2_x4 = document.getElementById('p2-x4').value;
+    var p2_x5 = document.getElementById('p2-x5').value;
+    var p2_y1 = document.getElementById('p2-y1').value;
+    var p2_y2 = document.getElementById('p2-y2').value;
+    var p2_y3 = document.getElementById('p2-y3').value;
+    var p2_y4 = document.getElementById('p2-y4').value;
+    var p2_y5 = document.getElementById('p2-y5').value;
+
+    var resultado = (p2_x1 * p2_y1) + (p2_x2 * p2_y2) + (p2_x3 * p2_y3) + (p2_x4 * p2_y4) + (p2_x5 * p2_y5);
+
+
+    document.querySelector('#p2-output').textContent = resultado; 
+
+}
 
 function problema3(){
     //definir un albeto
